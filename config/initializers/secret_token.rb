afabc6603884e13cc4f90a2e6fe699e2a4a71804
Minh Tien <minhtien@ubuntu.ubuntu-domain>
2013-06-20 15:01:13 +0700

    minh tien

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Minu::Application.config.secret_token = '87acdbc7102fafc397540778bed657ff9536a1e156618ed8097c9fccc4289f62bc76d35c673e8b6e5f86d2bbac9e53540f6ee0ebdf2455064fcd53c577f4cf8f'
